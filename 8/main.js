28a04d92db922621374f265c8967d36547194366
Marta1607 <71120060+Marta1607@users.noreply.github.com>
2022-05-12 14:40:18 +0200

    Add files via upload

---
+++ 8/main.js
@@ -0,0 +1,46 @@
+let prawy = document.getElementById('prawy')
+
+function zmiana1() {
+    prawy.style.backgroundColor = 'indigo'
+}
+
+function zmiana2() {
+    prawy.style.backgroundColor = 'SteelBlue'
+}
+
+function zmiana3() {
+    prawy.style.backgroundColor = 'Olive'
+}
+
+let hi = document.getElementById('hihi')
+
+function zmiana4() {
+    hihi.style.color = lista.value
+}
+
+function zmiana5() {
+    hihi.style.fontSize = proc.value
+}
+
+function zmiana6() {
+
+    if(ramka.checked){
+        obraz.style.border = '1px solid white'
+    }else {
+          obraz.style.border = 'None'  
+    }
+
+
+}
+
+function zmiana7() {
+    ull.style.listStyleType = 'disc'
+}
+
+function zmiana8() {
+    ull.style.listStyleType = 'square'
+}
+
+function zmiana9() {
+    ull.style.listStyleType = 'circle'
+}
